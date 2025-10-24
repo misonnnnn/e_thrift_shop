bd8ba0b15ca60cd25f30ca48ef4be22873462e92
john <misonjohnemmanuel@gmail.com>
2025-10-25 04:06:57 +0800

    add database mysql2 connection

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const db = require('./db');
 app.use(express.json());
 
-//routes
+//api routes
 app.get('/test', (req, res)=>{
     return res.status(200).json({
         'success' : true,
         'data' : 'success api'
     })
 })
+
+
 
 const PORT = process.env.API_PORT || 5000;
 const BASE_PATH = process.env.API_BASE_PATH || `http://localhost:${PORT}`;
