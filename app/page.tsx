c79caf17a39432fbd4d641be7b217de88e3d0b1c
john <misonjohnemmanuel@gmail.com>
2025-10-31 00:46:59 +0800

    home page responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
 		<>
 
 			{/* nav */}
-			<div className="container">
-				<div className="w-100 p-5 d-flex justify-content-between align-items-center">
-					<div className="d-flex justify-content-around align-items-center">
+			<div className="container ">
+				<div className="w-100 py-3 px-lg-5 px-md-3 px-sm-0 px-0 d-flex justify-content-between align-items-center">
+					<div className="main_menu d-flex justify-content-around align-items-center">
 						<div className="me-3 fw-bold">
 							<Link className="text-dark" href="/products/category/man">Man</Link>
 						</div>
@@ -20,6 +20,12 @@ export default function Home() {
 						<div className="me-3 fw-bold">
 							<Link className="text-dark" href="/products/category/sale">Sale</Link>
 						</div>
+					</div>
+
+					<div className="me-3 hamburger_menu">
+						<div className="hamburger_menu_line hamburger_menu_line1"></div>
+						<div className="hamburger_menu_line hamburger_menu_line2"></div>
+						<div className="hamburger_menu_line hamburger_menu_line3"></div>
 					</div>
 
 					<div>
@@ -43,10 +49,10 @@ export default function Home() {
 
 			{/* body */}
 
-			<div className="container">
+			<div className="container ">
 				<div className="w-100">
 					<div className="row m-0">
-						<div className="col-6">
+						<div className="col-lg-6 col-md-12">
 							<div>
 								<p className="home_text_2">TRENDY COLLECTION'S</p>
 								<p className="home_text_1">DRESSED TO BE NOTICED.</p>
@@ -57,13 +63,14 @@ export default function Home() {
 								</a>
 							</div>
 						</div>
-						<div className="col-6 ">
+						<div className="col-lg-6 col-md-12">
 							<div className="relative ">
 								<Image className="relative"
 									src="/res_main.png"   // File in public/images/
 									alt="My banner"
 									width={600}
 									height={600}
+									 style={{ width: "100%", height: "auto" }}
 								/>
 							</div>
 							
@@ -86,7 +93,7 @@ export default function Home() {
 							<p className="fw-bold"><FontAwesomeIcon icon={faSliders} /> FEATURED</p>
 							<div className="featured_product_outerdiv border-radius-top-left-20 p-3">
 								<div className="row m-0">
-									<div className="col-4 position-relative py-3">
+									<div className="col-lg-4 position-relative py-3">
 										<div className="d-flex border-end border-1 border-light">
 											<div className="position-relative">
 												<div className="featured_product_backdesign1"></div>
@@ -106,29 +113,8 @@ export default function Home() {
 										</div>
 									</div>
 									
-									
-									<div className="col-4 position-relative py-3">
-										<div className="d-flex border-end border-1 border-light">
-											<div className="position-relative">
-												<div className="featured_product_backdesign1"></div>
-												<Image className="featured_product_image"
-													src="/featured_products/2.png"   // File in public/images/
-													alt=""
-													width={150}
-													height={150}
-												/>
-											</div>
-											<div className="featured_product_details align-items-center d-flex">
-												<div>
-													<p className="featured_product_text">HOODIE</p>
-													<div className="featured_product_text_subtext">See All</div>
-												</div>
-											</div>
-										</div>
-									</div>
 
-
-									<div className="col-4 position-relative py-3">
+									<div className="col-lg-4 position-relative py-3">
 										<div className="d-flex">
 											<div className="position-relative">
 												<div className="featured_product_backdesign1"></div>
