2fd57ae6009fc2cefd10827ab2856024ac3eacc5
john <misonjohnemmanuel@gmail.com>
2025-11-02 23:10:44 +0800

    reconstructed files

--- fontawesome.js
+++
@@ -1,3 +0,0 @@
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false; // Tell Font Awesome to NOT insert its CSS automatically
