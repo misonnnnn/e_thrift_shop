2e99b1a631dc3992e492f2bd6e8f7d91f2fb2e02
john <misonjohnemmanuel@gmail.com>
2025-11-04 00:07:14 +0800

    adjust cors

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b19b47cfb328c92e94cc8e4489655544827678efc5368144d8111997ac4aebdb
-size 823
+oid sha256:da303095f0cb7f943cdb68a42c73199e79794023a7e60ee340c06d45c7289b72
+size 893
